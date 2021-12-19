9bcfc667f1cf4123ed92844bc5264a5782b25a62
anoctree <anoctree@gmail.com>
2021-12-20 00:16:23 +0330

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    res.send(`
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button>Sign up</button>
+        </form>
+
+    `);
+});
+
+app.post("/", async (req, res)=>{
+    const {email, password, passwordConfirmation} = req.body;
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+        return res.send("Email in use");
+    }
+    if (password !== passwordConfirmation) {
+        return res.send("Passwords must match");
+    }
+    res.send('Account created');
+
+});
+
+app.listen(3000, ()=>{
+    console.log("Listening");
+});
