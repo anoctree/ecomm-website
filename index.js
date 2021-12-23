d9214e0e99acb83657fca298ac1a58bbfbbb1c77
anoctree <anoctree@gmail.com>
2021-12-23 20:44:30 +0330

    Added sign-in and sign-out pages

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
+const { send } = require("express/lib/response");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+    keys: ['df32fqwldkqp23']
+}));
 
-app.get('/', (req, res)=>{
+app.get('/signup', (req, res)=>{
     res.send(`
+        Your id is ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email" />
             <input name="password" placeholder="password" />
@@ -17,7 +23,8 @@ app.get('/', (req, res)=>{
     `);
 });
 
-app.post("/", async (req, res)=>{
+
+app.post("/signup", async (req, res)=>{
     const {email, password, passwordConfirmation} = req.body;
     const existingUser = await usersRepo.getOneBy({ email });
     if (existingUser) {
@@ -26,8 +33,38 @@ app.post("/", async (req, res)=>{
     if (password !== passwordConfirmation) {
         return res.send("Passwords must match");
     }
+    const user = await usersRepo.create({ email, password });
+    req.session.userId = user.id;
     res.send('Account created');
+});
 
+app.get("/signin", (req, res)=>{
+    res.send(`
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+
+    `);
+});
+
+app.post("/signin", async (req, res)=>{
+    const {email, password} = req.body;
+    const user = await usersRepo.getOneBy({ email });
+    if (!user) {
+        return res.send("Email not found");
+    }
+    if (password !== user.password) {
+        return res.send("Incorrect password");
+    }
+    req.session.userId = user.id;
+    res.send("You are singed in");
+});
+
+app.get("/signout", (req, res)=>{
+    req.session = null;
+    res.send("You are signed out");
 });
 
 app.listen(3000, ()=>{
